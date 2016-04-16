b7bf8828dc695420960e82fe13e7b749d36a5178
lizhipower <lizhipower@gmail.com>
2016-04-16 21:50:15 +0800

    增加了一个判断数据类型的小例子

--- src/components/StringDemos/index.js
+++ src/components/StringDemos/index.js
@@ -2,12 +2,15 @@
  * Created by ZhiLI on 2016/3/27.
  * Email: lizhipower@gmail.com
  */
+
+
 export default class StringDemos extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       string: '2323',
-      rslt: ''
+      rslt: '',
+      similarRslt: false
     };
   }
   doShow(strTar) {
@@ -59,8 +62,6 @@ export default class StringDemos extends React.Component {
     let str = this.state.string;
     let strArr = str.split('');
     strArr = strArr.sort();
-    console.log(strArr);
-
     let strTar = strArr[0].toString();
     for (let i = 1; i < strArr.length; i++) {
       if (strArr[i - 1] !== strArr[i]) {
@@ -73,35 +74,135 @@ export default class StringDemos extends React.Component {
     let array = this.state.string;
     let strTar = '';
     for (let i = 0, l = array.length; i < l; i++) {
-      console.log('==============');
-
-      console.log('loop 1 i', i);
       for (let j = i + 1; j < l; j++) {
-        console.log('--------------');
-
         if (array[i] === array[j]) {
-          console.log('loop 2-1 i', i);
-          console.log('loop 2-1 j', j);
           j = ++i;
-          console.log('loop 2-2 i', i);
-          console.log('loop 2-2 j', j);
         }
       }
       strTar = strTar + array[i];
     }
     this.doShow(strTar);
   }
+  getType(e) {
+    let type;
+    if (e === null) {
+      type = '[object Null]';
+    } else if (e === undefined) {
+      type = '[object Undefined]';
+    } else if (e === window) {
+      type = '[object Window]';
+    } else {
+      type = Object.prototype.toString.apply(e);
+    }
+    return type;
+  }
+  doSimilar(cases) {
+    class RsltObj {
+      constructor() {
+        let obj = {
+          '[object Null]': 0,
+          '[object Undefined]': 0,
+          '[object Window]': 0,
+          '[object Date]': 0,
+          '[object String]': 0,
+          '[object Boolean]': 0,
+          '[object Number]': 0,
+          '[object Function]': 0
+        };
+        for (let props in obj) {
+          this[props] = obj[props];
+        }
+      }
+    }
+
+    let rsltArr = cases.map(obj => {
+      let rsltObj1 = new RsltObj();
+      let rsltObj2 = new RsltObj();
+      let result;
+
+      obj.arr1.forEach(e => {
+        rsltObj1[this.getType(e)] = rsltObj1[this.getType(e)] + 1;
+      });
+      obj.arr2.forEach(e => {
+        rsltObj2[this.getType(e)] = rsltObj2[this.getType(e)] + 1;
+      });
+      for (let p in rsltObj1) {
+        if (rsltObj1[p] !== rsltObj2[p]) {
+          result = false;
+          break;
+        } else {
+          result = true;
+        }
+      }
+      return result === obj.expect;
+    });
+    console.log(rsltArr);
+  }
+  doCal() {
+    let cases = [
+      {
+        arr1: [1, true, null],
+        arr2: [null, false, 100],
+        expect: true
+      },
+      {
+        arr1: [function(){}, 100],
+        arr2: [100, {}],
+        expect: false
+      },
+      {
+        arr1: [null, 999],
+        arr2: [{}, 444],
+        expect: false
+      },
+      {
+        arr1: [window, 1, true, new Date(), 'hahaha', (function(){}), undefined],
+        arr2: [undefined, (function () {}), 'okokok', new Date(), false, 2, window],
+        expect: true
+      },
+      {
+        arr1: [new Date()],
+        arr2: [{}],
+        expect: false
+      },
+      {
+        arr1: [window],
+        arr2: [{}],
+        expect: false
+      },
+      {
+        arr1: [undefined, 1],
+        arr2: [null, 2],
+        expect: false
+      },
+      {
+        arr1: [{}, {}, {}],
+        arr2: [{}, {}, null],
+        expect: false
+      }
+    ];
+
+    this.doSimilar(cases);
+  }
   render() {
     return (
       <div>
-        <p><em>{ this.state.string }</em></p>
-        <p><em>{ this.state.rslt }</em></p>
-        <button onClick={ e => this.doUnique1(e) }>Unique1</button>
-        <button onClick={ e => this.doUnique2(e) }>Unique2</button>
-        <button onClick={ e => this.doUnique3(e) }>Unique3</button>
-        <button onClick={ e => this.doUnique4(e) }>Unique4</button>
-        <button onClick={ e => this.doUnique5(e) }>Unique5</button>
+        <section>
+          <p><em>{ this.state.string }</em></p>
+          <p><em>{ this.state.rslt }</em></p>
+          <button onClick={ e => this.doUnique1(e) }>Unique1</button>
+          <button onClick={ e => this.doUnique2(e) }>Unique2</button>
+          <button onClick={ e => this.doUnique3(e) }>Unique3</button>
+          <button onClick={ e => this.doUnique4(e) }>Unique4</button>
+          <button onClick={ e => this.doUnique5(e) }>Unique5</button>
+        </section>
+        <section>
+          <p><em>{ this.state.similarRslt.toString() }</em></p>
+          <button onClick={ e => this.doCal(e) }>Similar</button>
+        </section>
       </div>
     );
   }
 }
+
+
